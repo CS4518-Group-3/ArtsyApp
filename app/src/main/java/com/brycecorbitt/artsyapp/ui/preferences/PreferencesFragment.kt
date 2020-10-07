9ec5846f222a2020715078b9fc9e85591c000e48
Ken Desrosiers <kdesrosiers@wpi.edu>
2020-10-06 20:45:28 -0400

    initial preferences ui

--- app/src/main/java/com/brycecorbitt/artsyapp/ui/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/brycecorbitt/artsyapp/ui/preferences/PreferencesFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.brycecorbitt.artsyapp.R
 
@@ -22,10 +20,6 @@ class PreferencesFragment : Fragment() {
         settingsViewModel =
                 ViewModelProviders.of(this).get(PreferencesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_preferences, container, false)
-        val textView: TextView = root.findViewById(R.id.text_preferences)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
--- app/src/main/java/com/brycecorbitt/artsyapp/ui/preferences/PreferencesViewModel.kt
+++ app/src/main/java/com/brycecorbitt/artsyapp/ui/preferences/PreferencesViewModel.kt
@@ -4,10 +4,4 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class PreferencesViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is preferences Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class PreferencesViewModel : ViewModel() {}
