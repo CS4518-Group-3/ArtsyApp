42bd90ae7382979783637ad486ce7f05690a8985
Bryce Corbitt <bscorbitt@wpi.edu>
2020-10-15 20:47:07 -0400

    Added global reference to current location
    Signed-off-by: Bryce Corbitt <bscorbitt@wpi.edu>

--- app/src/main/java/com/brycecorbitt/artsyapp/LocationService.kt
+++ app/src/main/java/com/brycecorbitt/artsyapp/LocationService.kt
@@ -3,6 +3,7 @@ package com.brycecorbitt.artsyapp
 import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
+import android.location.Location
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
@@ -25,6 +26,7 @@ class LocationService : Service() {
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult : LocationResult) {
                 super.onLocationResult(locationResult)
+                current_location = locationResult.lastLocation
                 Log.d("LATLON", locationResult.lastLocation.latitude.toString() + ", " + locationResult.lastLocation.longitude.toString())
                 //Toast.makeText(applicationContext, locationResult.lastLocation.latitude.toString() + ", " + locationResult.lastLocation.longitude.toString(), Toast.LENGTH_LONG).show()
             }
@@ -46,5 +48,9 @@ class LocationService : Service() {
         locationRequest.interval = 5000
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
         client.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+    }
+
+    companion object {
+        var current_location: Location? = null
     }
 }
